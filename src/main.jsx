d9edd3477925a2e17f114a56e90aa17bb1b50a9a
Kanishk19092003 <kanishkyadav141@gmail.com>
2026-07-16 20:43:21 +0530

    Initialized routes and context

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import RecipeContex from "./context/RecipeContex.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <RecipeContex>
     <BrowserRouter>
-    <App />
-    <ToastContainer/>
-    </BrowserRouter>,
-)
+      <App />
+      <ToastContainer />
+    </BrowserRouter>
+  </RecipeContex>,
+);
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,72 @@
+import { nanoid } from "nanoid";
+import { useForm } from "react-hook-form";
 
 const Create = () => {
-  return (
-    <div>Create</div>
-  )
-}
+  const { register, handleSubmit } = useForm();
 
-export default Create
+  const SubmitHandler = (data) =>{
+    data.id = nanoid();
+    console.log(data);
+    
+  }
+
+  return (
+    <form onSubmit={handleSubmit(SubmitHandler)}>
+      <input
+        className="block border-b outline-0 p-2"
+        {...register("image")}
+        type="url"
+        placeholder="Enter image url"
+      />
+
+      <small className="text-red-400">This is how error is shown</small>
+      
+      <input
+        className="block border-b outline-0 p-2"
+        {...register("title")}
+        type="text"
+        placeholder="Recipe title"
+      />
+
+      <input
+        className="block border-b outline-0 p-2"
+        {...register("Chef")}
+        type="text"
+        placeholder="Recipe title"
+      />
+
+      <textarea
+        className="block border-b outline-0 p-2"
+        {...register("description")}
+        placeholder="Start from here"
+      ></textarea>
+
+      
+      <textarea
+        className="block border-b outline-0 p-2"
+        {...register("ingredients")}
+        placeholder="Start from here"
+      ></textarea>
+
+      
+      <textarea
+        className="block border-b outline-0 p-2"
+        {...register("instructions")}
+        placeholder="Start from here"
+      ></textarea>
+
+      <select
+        className="block border-b outline-0 p-2"
+        {...register("category")}
+      >
+        <option value="cat 1">Category 1</option>
+        <option value="cat 2">Category 2</option>
+        <option value="cat 3">Category 3</option>
+      </select>
+      
+      <button className="mt-5 block bg-gray-900 px-4 py-2 rounded">Save Recipe</button>      
+    </form>
+  );
+};
+
+export default Create;
